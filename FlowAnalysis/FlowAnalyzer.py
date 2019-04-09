8da50fd2ec781bb14ff77d91e028a67cbf8c291f
Andy Dolan <andy.dolan.dev@gmail.com>
2019-04-09 15:36:19 -0600

    Extract Flow to separate file

--- FlowAnalysis/FlowAnalyzer.py
+++ FlowAnalysis/FlowAnalyzer.py
@@ -1,5 +1,6 @@
 import json
 import statistics
+from FlowAnalysis._flow import Flow
 
 class FlowAnalyzer:
   """The FlowAnalyzer class.
@@ -64,32 +65,3 @@ class FlowAnalyzer:
       flow_collection.append(flow_to_append_to)
 
     flow_to_append_to.packets.append(pkt)
-
-class Flow:
-  """Simple representation of a flow.
-  Defined as a number of packets that share particular properties, per RFC 3917.
-  """
-
-  def __init__(self):
-    self.is_open = True
-    self.packets = []
-
-  def __repr__(self):
-    src_addr = self.packets[0].get('_source').get('layers').get('ip').get('ip.src')
-    dst_addr = self.packets[0].get('_source').get('layers').get('ip').get('ip.dst')
-
-    return '<Flow ({} <--> {}) of {} packets; Open: {}>'.format(src_addr, dst_addr, len(self.packets), self.is_open)
-
-  def get_avg_payload_length(self):
-    lens = [int(p.get('_source').get('layers').get('data', {'data.len': 0}).get('data.len')) for p in self.packets]
-    return statistics.mean(lens)
-
-  def get_bitrate(self):
-    times = [float(p.get('_source').get('layers').get('frame').get('frame.time_epoch')) for p in self.packets]
-    start_time = min(times)
-    end_time = max(times)
-
-    duration = end_time - start_time
-    aggregate_bytes = sum([float(p.get('_source').get('layers').get('frame').get('frame.len')) for p in self.packets])
-
-    return (aggregate_bytes / duration) * 8
