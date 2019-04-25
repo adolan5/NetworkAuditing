2454a44dbf4a2196475bb871e50306adc1705060
Andy Dolan <andy.dolan.dev@gmail.com>
2019-04-25 04:23:01 -0600

    Better repr for interactions

--- FlowAnalysis/_interaction.py
+++ FlowAnalysis/_interaction.py
@@ -3,11 +3,14 @@ class Interaction:
     self.packet_stats = pkt_stats if pkt_stats else []
 
   def __repr__(self):
-    return str(len(self.packet_stats))
+    return '<itx of {} pkts over {:.02f} sec>'.format(self.__len__(), self.get_duration())
 
   def __iter__(self):
     for p in self.packet_stats:
       yield p
+
+  def __len__(self):
+    return len(self.packet_stats)
 
   def __getitem__(self, key):
     return self.packet_stats[key]
@@ -23,3 +26,6 @@ class Interaction:
     if min(times) >= duration_start and max(times) <= duration_end:
       return self
     return Interaction([p for p in self.packet_stats if p.get('rel_time') >= duration_start and p.get('rel_time') <= duration_end])
+
+  def get_duration(self):
+    return self.packet_stats[-1].get('rel_time') - self.packet_stats[0].get('rel_time')
