294b25af5ad8fae6d4ddae5aef8b183a96142927
Andy Dolan <andy.dolan.dev@gmail.com>
2019-05-03 00:32:18 -0600

    Begin implementation of audit method

--- FlowAnalysis/NetAuditor.py
+++ FlowAnalysis/NetAuditor.py
@@ -4,6 +4,8 @@ from FlowAnalysis import FlowAnalyzer
 class NetAuditor:
   def __init__(self, pdp, capture):
     self.pdp = pdp
+    self.flow_analyzer = FlowAnalyzer()
+
     if type(capture) is str:
       with open(capture) as f:
         self._raw_packets = json.load(f)
@@ -13,4 +15,13 @@ class NetAuditor:
     self.audit()
 
   def audit(self):
-    pass
+    for p in self._raw_packets:
+      current_flow = self.flow_analyzer.append_packet(p)
+      if current_flow is None:
+        continue
+      print(p)
+      req = self._generate_request(current_flow)
+
+  def _generate_request(self, flow):
+    print(flow.packet_stats[-1])
+    return None
