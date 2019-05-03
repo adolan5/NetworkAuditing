262730ec016fcb77109e1401fb2fe07125c2be84
Andy Dolan <andy.dolan.dev@gmail.com>
2019-05-03 01:14:35 -0600

    Generate a first request object

--- FlowAnalysis/NetAuditor.py
+++ FlowAnalysis/NetAuditor.py
@@ -19,9 +19,32 @@ class NetAuditor:
       current_flow = self.flow_analyzer.append_packet(p)
       if current_flow is None:
         continue
-      print(p)
       req = self._generate_request(current_flow)
+      print(req)
 
   def _generate_request(self, flow):
-    print(flow.packet_stats[-1])
-    return None
+    current_packet = flow.packet_stats[-1]
+    flow_stats = flow.get_aggregate_stats()
+    itx_stats = flow.interactions[-1].get_aggregate_stats()
+    req = {
+        'packet': {
+          'src_addr': current_packet.get('src_addr'),
+          'dst_addr': current_packet.get('dst_addr'),
+          'payload_len': current_packet.get('pkt_len'),
+          'relative_time': current_packet.get('rel_time')
+          },
+        'network_state': {
+          'current_flow': {
+            'duration': flow_stats.get('duration'),
+            'num_interactions': flow_stats.get('num_interactions'),
+            'total_payload_bytes': flow_stats.get('total_bytes'),
+            'total_bytes_by_source': flow_stats.get('total_by_src')
+            },
+          'current_interaction': {
+            'duration': itx_stats.get('duration'),
+            'total_bytes': itx_stats.get('total_bytes'),
+            'total_bytes_by_source': itx_stats.get('total_lens')
+            }
+          }
+        }
+    return req
